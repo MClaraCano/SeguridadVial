c0644a5553b15c60a3eb0bce501bb0fc410762ab
Clara Cano <mclara.cano@gmail.com>
2023-03-23 12:50:41 -0300

    ahora si: subiendo mod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Auto auto = new Auto(true, 120);
-        Conductor conductor = new Conductor("Federico", auto, false);
+        Conductor conductor = new Conductor("Carlos", auto, false);
 
         System.out.println("");
         System.out.println("¿El auto de " + conductor.getNombre() + " es seguro?");
